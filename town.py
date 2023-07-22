4bb382753d490204fb776a8a0b4743e50149f68b
aVastAbyss <140279552+aVastAbyss@users.noreply.github.com>
2023-07-22 17:33:18 -0500

    Create town.py

---
+++ town.py
@@ -0,0 +1,34 @@
+import os
+import random
+
+def prompt_user():
+
+    print("1. Buy supplies")
+    print("2. Stop to rest")
+    print("3. Continue on the trail")
+    user_action = input("Enter the number associated with the action you wish to take: ")
+
+    return user_action
+
+def enter_town(inventory, cash):
+
+    print("You have just arrived at a small town! What do you wish to do?")
+
+    while True:
+
+        user_action = prompt_user()
+
+        if user_action == "1":
+            inventory, cash = buy_supplies(inventory, cash)
+
+        elif user_action == "2":
+            
+
+        elif user_action == "3":
+            distance_to_town = spawn_next_town()
+            return inventory, cash, distance_to_town
+    
+        else:
+            print("Invalid input. Try again")
+    
+        
