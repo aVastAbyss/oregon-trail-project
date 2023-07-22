f81ed699e1725b4843820ce4edccb04308471ce2
aVastAbyss <140279552+aVastAbyss@users.noreply.github.com>
2023-07-22 17:49:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
 
     current_trail_distance = 0
     distance_to_town = 0
+    day = 1
 
     cash = 0
     inventory = {"oxen": 0,
@@ -14,19 +15,19 @@ def main():
                  "medical supplies": 0}
 
     person_1 = {"name": "",
-                "health": 0}
+                "health": 100}
 
     person_2 = {"name": "",
-                "health": 0}
+                "health": 100}
 
     person_3 = {"name": "",
-                "health": 0}
+                "health": 100}
 
     person_4 = {"name": "",
-                "health": 0}
+                "health": 100}
 
     person_5 = {"name": "",
-                "health": 0}
+                "health": 100}
 
     party_list = [person_1, person_2, person_3, person_4, person_5]
 
@@ -34,6 +35,6 @@ def main():
     party_list = name_companions(party_list)
 
     if distance_to_town == 0:
-        inventory, distance_to_town = enter_town(inventory)
+        inventory, cash, day, distance_to_town = enter_town(inventory, cash, day)
 
 main()
