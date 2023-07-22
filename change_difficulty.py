9e61acd31cba7c81826d88755bcb61a78443fae0
Flamgapper <128209670+Flamgapper@users.noreply.github.com>
2023-07-22 17:18:33 -0500

    Update change_difficulty.py

--- change_difficulty.py
+++ change_difficulty.py
@@ -1,3 +1,21 @@
 def change_difficulty():
-
+    print("""Choose from one of the difficulties below:
+    1. Easy
+    2. Medium
+    3. Hard
+    """)
+    user_choice = input("Choose a number corresponding to one of the options above: ")
+    while True:
+        match user_choice:
+            case "1":
+                cash = 6000
+                break
+            case "2":
+                cash = 2500
+                break
+            case "3":
+                cash = 1000
+                break
+            case _:
+                user_choice = input("Please enter a valid number corresponding to one of the options above: ")
     return cash
