eee193d82cd53803cfaf76eb0a390375ce2ffdbf
aVastAbyss <140279552+aVastAbyss@users.noreply.github.com>
2023-07-24 18:09:54 -0500

    Update name_companions_alt.py

--- name_companions_alt.py
+++ name_companions_alt.py
@@ -9,28 +9,20 @@ def validate_names():
 
     while True:
         is_names_correct = input("Are the above names correct (y/n)? ").lower()
-        
+
         if is_names_correct == "y" or is_names_correct == "n":
             return is_names_correct
 
         print("Invalid input.")
-
-def is_valid_name_change(name_index):
-
-    valid_inputs_list = ["1", "2", "3", "4", "5"]
-    for valid_input in valid_inputs_list:
-
-        if name_index == valid_input:
-            return True
-
-    return False
 
 def prompt_name_change(party_list):
 
     while True:
         name_index = input("Which name do you wish to change (1-5)? ")
 
-        if is_valid_name_change(name_index):
+        valid_inputs_list = ["1", "2", "3", "4", "5"]
+
+        if name_index in valid_inputs_list:
             name_index = int(name_index) - 1
             break
 
