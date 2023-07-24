e46894b790fbf3a8dda32a4d2b5a75feade7ca1c
Skamayy <140280714+Skamayy@users.noreply.github.com>
2023-07-23 19:09:44 -0500

    Update name_companions.py

--- name_companions.py
+++ name_companions.py
@@ -33,20 +33,34 @@ def name_companions(party_list):
         if confirm_names == "Y" or confirm_names == "y" or confirm_names == "Yes" or confirm_names == "yes":
             changes_confirmed = True
 
-        if confirm_names == "N" or confirm_names == "n" or confirm_names == "No" or confirm_names == "no":
-            change_name = input("Which name is incorrect? (1-5) ")
+        while changes_confirmed == False:
 
-             choices = ["1", "2", "3", "4", "5"]
+            if confirm_names == "N" or confirm_names == "n" or confirm_names == "No" or confirm_names == "no":
+                change_name = input("Which name is incorrect? (1-5) ")
 
-            for choice in choices:
+                choices = ["1", "2", "3", "4", "5"]
+
+                for choice in choices:
+                    
+                    if choice == change_name and change_name == "1":
+                        member_1["name"] = input("What is the name of the party leader? ")
+                        print("1." + member_1["name"] + "\n2." + member_2["name"] + "\n3." + member_3["name"] + "\n4." + member_4["name"] + "\n5." + member_5["name"])
+                        break
+                    
+                    elif choice == change_name:
+
+                        change_name = int(change_name)
+
+                        for member in party_list[change_name - 1:]:
+                                member["name"] = input("What are the names of the other members of your party? ")
+                                print("1." + member_1["name"] + "\n2." + member_2["name"] + "\n3." + member_3["name"] + "\n4." + member_4["name"] + "\n5." + member_5["name"])
+                                break
+
                 
-                if choice == change_name and change_name == "1":
-                    member_1["name"] = input("What is the name of the party leader? ")
-                    break
 
-                for member in party_list[1:]:
-                    if choice == change_name:
-                        member["name"] = input("What are the names of the other members of your party? ")
+                confirm_names = input("Are these names correct? (Y/N) ")
+
+
     print(party_list)
     return party_list
 
